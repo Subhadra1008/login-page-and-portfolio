ae1d0643f7f7b3a313710d94826049ce9ecb4e21
Subhadra1008 <ksubhadra941@gmail.com>
2025-08-15 00:47:10 +0530

    Add files via upload

---
+++ scnner.py
@@ -0,0 +1,24 @@
+import qrcode
+
+# Input UPI ID
+upi_id = input("Enter your UPI ID: ")
+
+# Define payment URLs for each app
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+# Generate QR codes
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+# Save QR codes as images
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+# Display QR codes
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
